56d9293a07803481881355b1759a8653a95ac0c7
brmendez <brmendez@gmail.comgit statuscdlsgit statusgit help -agit credential-osxkeychainexit:wqgitcd .lsgitgit config --global user.email brmendez@gmail.com password: BrMe002!git config --global user.email brmendez@gmail.com>
2014-08-19 21:40:35 -0700

    On Slide 14. -added UIImage View -added 2 UITextFields -added National Album Cover -added appropriate delegations

--- textFieldDemoWClass/ViewController.swift
+++ textFieldDemoWClass/ViewController.swift
@@ -8,16 +8,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-                            
+class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate {
+    
+//MARK: Outlets
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var firstNameTextField: UITextField!
+    @IBOutlet weak var lastNameTextField: UITextField!
+    
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
+    
+    
+    
+    
+    
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
