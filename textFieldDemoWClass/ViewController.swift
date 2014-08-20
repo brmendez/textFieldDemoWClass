a3c63583a163fa46615d5c062e1a5634661f9db0
brmendez <brmendez@gmail.comgit statuscdlsgit statusgit help -agit credential-osxkeychainexit:wqgitcd .lsgitgit config --global user.email brmendez@gmail.com password: BrMe002!git config --global user.email brmendez@gmail.com>
2014-08-20 13:36:37 -0700

    finished demo.

--- textFieldDemoWClass/ViewController.swift
+++ textFieldDemoWClass/ViewController.swift
@@ -8,34 +8,55 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate {
+class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
 //MARK: Outlets
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var firstNameTextField: UITextField!
     @IBOutlet weak var lastNameTextField: UITextField!
     
-    
-    
-    
-    
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()    }
-
-    
-    
-    
-    
-    
-    
-    
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
 
+    var albumCoverNational = UIImage(named: "theNational.jpg")
+    
+
+    func textFieldShouldReturn(textField: UITextField!) -> Bool {
+        textField.resignFirstResponder()
+        return true
+        
+    }
+    
+    @IBAction func photoButtonPressed(sender: AnyObject) {
+        
+        var imagePickerController = UIImagePickerController()
+        
+        imagePickerController.delegate = self
+        imagePickerController.allowsEditing = true
+        
+        imagePickerController.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
+        self.presentViewController(imagePickerController, animated: true, completion: nil)
+        
+    }
+    
+    
+    func imagePickerController(picker: UIImagePickerController!, didFinishPickingMediaWithInfo info: [NSObject : AnyObject]!) {
+        picker.dismissViewControllerAnimated(true, completion: nil)
+        
+        var editedImage = info[UIImagePickerControllerEditedImage] as UIImage
+        self.imageView.image = editedImage
+        
+    }
+ 
+
+        
+    func imagePickerControllerDidCancel(picker: UIImagePickerController!) {
+        picker.dismissViewControllerAnimated(true, completion: nil)
+    }
 
 }
 
